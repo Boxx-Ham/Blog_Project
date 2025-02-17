559b8846441b50be42e4ef6d13b53434ac05cee3
Boxx-Ham <sangja234@gamil.com>
2025-02-17 11:00:40 +0900

    2025-02-17 test.http 위치 변경(test -> main) / JPA 메서드 테스트

--- src/main/java/boxx_ham/Blog_Project/MemberRepository.java
+++ src/main/java/boxx_ham/Blog_Project/MemberRepository.java
@@ -1,8 +1,11 @@
 package boxx_ham.Blog_Project;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByName(String name);   // SELECT * FROM member WHERE name = ?;
 }
---
+++ src/test/resources/insert-members.sql
@@ -0,0 +1,3 @@
+INSERT INTO member (id, name) VALUES (4, 'A');
+INSERT INTO member (id, name) VALUES (5, 'B');
+INSERT INTO member (id, name) VALUES (6, 'C');
