7d35bd5fc9ba4f8a166c2761483875799442bcd5
Boxx-Ham <sangja234@gamil.com>
2025-02-24 22:26:22 +0900

    2025-02-24 BoxxHam : link 태그 오타 수정 (lef -> rel) 및 7.5 수정/생성 기능 추가하기 ~ 7.6 생성 기능 마무리하기 -> 7장 블로그 화면 구성하기 끝

--- src/main/java/boxx_ham/Blog_Project/controller/BlogViewController.java
+++ src/main/java/boxx_ham/Blog_Project/controller/BlogViewController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import boxx_ham.Blog_Project.domain.Article;
 import boxx_ham.Blog_Project.dto.ArticleListViewResponse;
@@ -33,5 +34,23 @@ public class BlogViewController {
         model.addAttribute("article", new ArticleViewResponse(article));
 
         return "article";   // article.html 뷰 조회
+    }
+
+    // 새 글 생성 및 수정
+    @GetMapping("/new-article")
+    // id 키를 가진 쿼리 파라미터의 값을 id 변수에 매핑(id는 없을 수도 있음)
+    // id를 인식하지 못해 명시적으로 선언
+    public String newArticle(@RequestParam(name="id", required=false) Long id, Model model) {
+        // id가 없으면 새 글 생성
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+
+        // id가 있으면 글 수정
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";   // newArticle.html 뷰 조회
     }
 }
